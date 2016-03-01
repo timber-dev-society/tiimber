551277eed53f640ec6b8017916c8d4cf4ad67bc0
Dufreche <n.dufreche@gmail.com>
2016-03-01 15:21:36 +0000

    updating cropText function

--- src/Utility.php
+++ src/Utility.php
@@ -3,6 +3,12 @@ namespace KissPHP;
 
 class Utility
 {
+  /**
+   * Slugify text by removing non ASCII carac
+   * 
+   * @param $text String to slugify
+   * @return Sting
+   */
   public static function slugify($text)
   {
     $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
@@ -17,9 +23,33 @@ class Utility
     return $text;
   }
 
-  public static function cropText($text, $maxSize)
+  /**
+   * Crop text at the max size of your choice
+   * 
+   * @param $text String to crop
+   * @param $maxSize Integer the max size of the string
+   * @param $preserveWord Boolean the max size of the string
+   * @return String
+   */
+  public static function cropText($text, $maxSize, $preserveWords = true)
   {
-    return strlen($text) > $maxSize ? substr($text, 0, $maxSize) . '...' : $text;
+    if ($preserveWords) {
+      $words = explode(' ', $text);
+      $count = 0;
+      foreach($words as $word) {
+        $count += strlen($word) + 1;
+
+        if ($count >= ($maxSize + 1)) {
+          break;
+        }
+      }
+      
+      $maxSize = $count + 2;
+    }
+    
+    return strlen($text) > ($maxSize)
+      ? substr($text, 0, $maxSize - 3) . '...' 
+      : $text;
   }
 
   public static function t($text)
--- tests/UtilityTest.php
+++ tests/UtilityTest.php
@@ -3,21 +3,31 @@ namespace KissPHP\Tests;
 
 use KissPHP\Utility;
 
-class UtilityTest extends \PHPUnit_Framework_TestCase
+use PHPUnit_Framework_TestCase;
+
+class UtilityTest extends PHPUnit_Framework_TestCase
 {
-    public function testSlugify()
-    {
-        $this->assertEquals('abc123', Utility::slugify('abc123'));
-        $this->assertEquals('abc123', Utility::slugify('AbC123'));
-        $this->assertEquals('aceun', Utility::slugify('âçéüñ'));
-        $this->assertEquals('a-b-c-1-2-3', Utility::slugify('a b\\c/1,2%3'));
-        $this->assertEquals('1', Utility::slugify(':;.,?1'));
-        $this->assertEquals('n-a', Utility::slugify(':;.,?'));
-    }
-    
-    public function testCropText()
-    {
-        $this->assertEquals('abc', Utility::cropText('abc', 4));
-        $this->assertEquals('ab...', Utility::cropText('abc', 2));
-    }
+  public function testSlugify()
+  {
+    $this->assertEquals('abc123', Utility::slugify('abc123'));
+    $this->assertEquals('abc123', Utility::slugify('AbC123'));
+    $this->assertEquals('aceun', Utility::slugify('âçéüñ'));
+    $this->assertEquals('a-b-c-1-2-3', Utility::slugify('a b\\c/1,2%3'));
+    $this->assertEquals('1', Utility::slugify(':;.,?1'));
+    $this->assertEquals('n-a', Utility::slugify(':;.,?'));
+  }
+  
+  public function testCropText()
+  {
+    $this->assertEquals('abcde', Utility::cropText('abcde', 5, false));
+    $this->assertEquals('ab...', Utility::cropText('abcdef', 5, false));
+    $this->assertEquals(5, strlen(Utility::cropText('abcdef gh', 5, false)));
+  }
+  
+  public function testCropTextWithPreseveWord()
+  {
+    $this->assertEquals('foo bar', Utility::cropText('foo bar', 7));
+    $this->assertEquals('foo...', Utility::cropText('foo bar baz', 2));
+    $this->assertEquals('foo bar...', Utility::cropText('foo bar baz', 6));
+  }
 }
