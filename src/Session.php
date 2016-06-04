00611fedf7c5420f1caae56821b9f7aa44551bac
Nicolas Dufreche <n.dufreche@gmail.com>
2016-06-04 16:33:57 +0200

    add types to Session

--- src/Session.php
+++ src/Session.php
@@ -16,7 +16,7 @@ class Session
    *
    * @return Session
    */
-  public static function load()
+  public static function load(): Session
   {
     if (!self::$instance) {
       self::$instance = new self();
@@ -31,7 +31,7 @@ class Session
    * @param mixed $default
    * @return mixed
    */
-  public function get($key, $default = null)
+  public function get(string $key, $default = null)
   {
     return isset($_SESSION[$key]) ? $_SESSION[$key] : $default;
   }
@@ -42,9 +42,11 @@ class Session
    * @param String $key
    * @param mixed $value
    */
-  public function set($key, $value)
+  public function set(string $key, $value): Session
   {
     $_SESSION[$key] = $value;
+
+    return $this;
   }
 
   /**
@@ -52,8 +54,10 @@ class Session
    *
    * @param $key
    */
-  public function destruct($key)
+  public function destruct(string $key): Session
   {
     unset($_SESSION[$key]);
+
+    return $this;
   }
 }
