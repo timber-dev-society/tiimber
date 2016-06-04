0a7a36d0db7aee9f7ee0ba534d8e258af241f0c1
Nicolas Dufreche <n.dufreche@gmail.com>
2016-06-04 16:41:58 +0200

    Updating Mailer

--- src/Mailer.php
+++ src/Mailer.php
@@ -3,6 +3,9 @@ namespace Tiimber;
 
 use Tiimber\Interfaces\MailDriverInterface;
 
+/**
+ * Basic class to create e-mails.
+ */
 class Mailer
 {
   private static $instance;
@@ -11,25 +14,29 @@ class Mailer
 
   private function __construct()
   {
-    $config = Config::get('mailer');
+    $config = Config::get('mailer', false);
 
     if (!$config) {
       throw new Exception('No mailer configuration');
     }
 
-    $driver = '\\' . $config->driver;
-    $this->driver = new $driver();
+    $driver = $config->get('driver', false);
+    if (!$driver) {
+      throw new Exception('No mail driver found');
+    }
 
+    $this->driver = new $driver();
     if (!$this->driver instanceof MailDriverInterface) {
       throw new Exception($driver . ' must implement MailDriverInterface');
     }
+
     $this->driver->setConfig($config);
   }
 
   /**
    * @return MailDriverInterface;
    */
-  public static function newMail()
+  public static function newMail(): MailDriverInterface
   {
     if (!self::$instance) {
       self::$instance = new self();
