4abd664ad111a45e6206b99c4521e4347cc17a75
Nicolas Dufreche <n.dufreche@gmail.com>
2016-06-04 16:30:50 +0200

    remove upload file from AbstractController

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Tiimber;
 
-use Tiimber\Utility;
 use Tiimber\Controller;
 
 abstract class AbstractController
@@ -18,11 +17,6 @@ abstract class AbstractController
     $this->renderer = $renderer;
   }
 
-  public function render(Array $arguments = null)
-  {
-    return $this->renderer->render($this->tpl, $arguments);
-  }
-
   public function beforeAction($action)
   {
   }
@@ -30,29 +24,5 @@ abstract class AbstractController
   public function redirect($location, array $args = [])
   {
     Controller::redirect($location, $args);
-  }
-
-  protected function uploadFile($post, $identifier, $directory = 'images')
-  {
-    $post->{$identifier . '_slug'} = empty($post->{$identifier . '_slug'}) ? Utility::slugify($post->{$identifier . '_title'}) : $post->{$identifier . '_slug'};
-    $message = '';
-    $target = Application::getResourceDir() . $directory . '/';
-
-    // traitement de l'upload
-    if (!empty($_FILES[$identifier]['name'])) {
-      $extension = pathinfo($_FILES[$identifier]['name'], PATHINFO_EXTENSION);
-      $img_name = $post->{$identifier . '_slug'} . '.' . $extension;
-
-      $finalName = $img_name;
-
-      if (file_exists($target . $finalName)) {
-        $rand = substr(sha1(rand()), 0 , 4);
-        $finalName = $post->{$identifier . '_slug'} . '-' . $rand . '.' . $extension;
-      }
-      $post->{$identifier} = $finalName;
-
-      return move_uploaded_file($_FILES[$identifier]['tmp_name'], $target . $finalName);
-    }
-    return false;
   }
 }
---
+++ src/Utilities/UploadFile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tiimber\Utilities;
+
+use Tiimber\Application;
+use Tiimber\Exception;
+/**
+ *  Utility helper to upload files
+ */
+trait UploadFile
+{
+  public function upload(string $field, string $name = null, string $directory = 'images'): string
+  {
+    $uploadDir = Application::getResourceDir() . $directory . '/';
+    if (empty($_FILES[$field]['name'])) {
+      throw Exception('500', $field . ' seems not to be an upload field');
+    }
+    $filepath = $uploadDir . $_FILES[$field]['name'];
+    if (!is_null($name)) {
+      $extension = pathinfo($_FILES[$identifier]['name'], PATHINFO_EXTENSION);
+      if (file_exists($uploadDir . $name . $extension)) {
+        $filepath = = $uploadDir . $name . '-' . substr(sha1(rand()), 0 , 4) . '.' . $extension;
+      }
+    }
+    if (!move_uploaded_file($_FILES[$field]['tmp_name'], $filepath)){
+      throw Exception('500', 'Unable to move uploaded file to ' . $filepath);
+    }
+    return $filepath;
+  }
+}
