6dc1f6000621b5d75d9e1b94e32d918386f8c281
Nicolas Dufreche <n.dufreche@gmail.com>
2017-03-11 16:01:24 +0100

    Updating tests/Traits/RouteResolverTraitTest.php

--- tests/Traits/RouteResolverTraitTest.php
+++ tests/Traits/RouteResolverTraitTest.php
@@ -1,9 +1,9 @@
 <?php
 namespace Tiimber\Tests\Traits;
 
-use Tiimber\Application;
-use Tiimber\Config;
-use Tiimber\Traits\RouteResolverTrait;
+use Tiimber\{Memory, Config, Traits\RouteResolverTrait};
+
+use const Tiimber\Consts\{Folder\CONFIG, Scopes\FOLDER};
 
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
@@ -18,8 +18,7 @@ class RouteResolverTraitTest extends PHPUnit_Framework_TestCase
 
   public function __construct()
   {
-    $application = new Application();
-    $application->setConfigDir(dirname(__DIR__) . '/Config/');
+    Memory::set(FOLDER)->set(CONFIG, dirname(__DIR__) . '/Config/');
     $this->mock = $this->getMockForTrait('Tiimber\Traits\RouteResolverTrait');
     $this->routes = Config::get('routes');
   }
@@ -27,21 +26,22 @@ class RouteResolverTraitTest extends PHPUnit_Framework_TestCase
   public function testResolving()
   {
     $match = $this->mock->resolve($this->routes, 'GET', '/');
-    $this->assertEquals('basic_route', $match['_route']);
+    $this->assertEquals('basic', $match['_route']);
+
     $match = $this->mock->resolve($this->routes, 'GET', '/foo');
-    $this->assertEquals('route_with_protocol', $match['_route']);
+    $this->assertEquals('with::protocol', $match['_route']);
   }
 
   public function testResolvingWithParameters()
   {
     $match = $this->mock->resolve($this->routes, 'GET', '/1');
-    $this->assertEquals('route_with_param', $match['_route']);
+    $this->assertEquals('with::param', $match['_route']);
     $this->assertEquals('1', $match['id']);
   }
 
   public function testResolvingNotFound()
   {
     $this->expectException(ResourceNotFoundException::class);
-    $match = $this->mock->resolve($this->routes, 'GET', '/this/is/a/404');
+    $this->mock->resolve($this->routes, 'GET', '/this/is/a/404');
   }
 }
